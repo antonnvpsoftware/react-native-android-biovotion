6b55106422ef08622bd45abdbe625256bbdfc0ca
Anton Koshovnik <anton@nvpsoftware.com>
2018-02-20 14:40:22 +0200

    summary
    first commit

---
+++ android/src/main/java/com/nvpsoftware/biovotion/NVPBiovotionPackage.java
@@ -0,0 +1,31 @@
+package com.nvpsoftware.biovotion;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NVPBiovotionPackage implements ReactPackage {
+    static final String TAG = "Biovotion";
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new NVPBiovotionModule(reactContext));
+        return modules;
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
